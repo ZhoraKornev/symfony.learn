a40a23a470f4d0f4d3b77f7400d86d75bd07ea7e
Serega <obuhov.s@toplyvo.app>
2020-03-27 15:35:37 +0200

    some refactor

--- manager/src/Model/Flusher.php
+++ manager/src/Model/Flusher.php
@@ -4,25 +4,7 @@ declare(strict_types=1);
 
 namespace App\Model;
 
-use Doctrine\ORM\EntityManagerInterface;
-
-class Flusher
+interface Flusher
 {
-    private $em;
-    private $dispatcher;
-
-    public function __construct(EntityManagerInterface $em, EventDispatcher $dispatcher)
-    {
-        $this->em = $em;
-        $this->dispatcher = $dispatcher;
-    }
-
-    public function flush(AggregateRoot ...$roots): void
-    {
-        $this->em->flush();
-
-        foreach ($roots as $root) {
-            $this->dispatcher->dispatch($root->releaseEvents());
-        }
-    }
+    public function flush(): void;
 }
