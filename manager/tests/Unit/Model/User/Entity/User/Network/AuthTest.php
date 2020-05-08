57d3988fa140fea252912f4ffeeb8d6dea0f546e
Серега Обухов <obuhov.s@toplyvo.app>
2020-05-08 21:00:57 +0300

    [part-4-2] user refactor

--- manager/tests/Unit/Model/User/Entity/User/Network/AuthTest.php
+++ manager/tests/Unit/Model/User/Entity/User/Network/AuthTest.php
@@ -13,38 +13,21 @@ class AuthTest extends TestCase
 {
     public function testSuccess(): void
     {
-        $user = new User(
-            Id::next(),
-            new \DateTimeImmutable()
-        );
-
-        $user->signUpByNetwork(
+        $user = User::signUpByNetwork(
+            $id = Id::next(),
+            $date = new \DateTimeImmutable(),
             $network = 'vk',
             $identity = '0000001'
         );
 
         self::assertTrue($user->isActive());
 
+        self::assertEquals($id, $user->getId());
+        self::assertEquals($date, $user->getDate());
+
         self::assertCount(1, $networks = $user->getNetworks());
         self::assertInstanceOf(Network::class, $first = reset($networks));
         self::assertEquals($network, $first->getNetwork());
         self::assertEquals($identity, $first->getIdentity());
-    }
-
-    public function testAlready(): void
-    {
-        $user = new User(
-            $id = Id::next(),
-            $date = new \DateTimeImmutable()
-        );
-
-        $user->signUpByNetwork(
-            $network = 'vk',
-            $identity = '0000001'
-        );
-
-        $this->expectExceptionMessage('User is already signed up.');
-
-        $user->signUpByNetwork($network, $identity);
     }
 }
