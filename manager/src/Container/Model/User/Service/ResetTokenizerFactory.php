77cd02d451b26f1cc6377a15c6b4bafc4245e0cf
Zhora <Zhora@i.ua>
2020-07-12 23:00:55 +0300

    [part-5-2} tokeniser

--- manager/src/Container/Model/User/Service/ResetTokenizerFactory.php
+++ manager/src/Container/Model/User/Service/ResetTokenizerFactory.php
@@ -5,12 +5,11 @@ declare(strict_types=1);
 namespace App\Container\Model\User\Service;
 
 use App\Model\User\Service\ResetTokenizer;
-use DateInterval;
 
 class ResetTokenizerFactory
 {
     public function create(string $interval): ResetTokenizer
     {
-        return new ResetTokenizer(new DateInterval($interval));
+        return new ResetTokenizer(new \DateInterval($interval));
     }
 }
